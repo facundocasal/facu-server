779dda73d388f60b7d8f6bef0fd8d7ff94bea6e2
Facundo Nahuel Casal <fcasal92@gmail.com>
2022-09-06 19:29:56 -0300

    initialize express server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 import express from "express";
 
 const app = express()
+
+app.get("/", (req , res)=>{
+    res.send(
+        {
+            mensage: "holaa"
+        }
+    )
+})
+
+app.listen(8000 , ()=>{
+    console.log("escuchando puero 8080")
+})
